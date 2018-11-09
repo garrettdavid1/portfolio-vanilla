1f3153bee671cc9cc2c707ff9ae66c54842bb603
David Garrett <garrettdavid1@gmail.com>
2018-11-08 23:26:42 -0500

    Implemented typewriter for welcome message, started working on header

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 function run() {
-    console.log('DOM is ready.');
+	
+	typeWelcomeMessage();
+
+	// typeWriter("Hello, I'm David.", document.getElementById('message'));
 }
 
 if (document.readyState!='loading'){
@@ -16,3 +19,47 @@ if (document.readyState!='loading'){
     document ready listener from
     https://plainjs.com/javascript/events/running-code-when-the-document-is-ready-15/
 */
+
+typeWriter = (text, component, speed = 50, i = 0, callback) => {
+	component.innerHTML += text.charAt(i);
+    if (i < text.length) {
+        i++;
+        setTimeout(() => {
+            this.typeWriter(text, component, speed, i, callback);
+        }, speed);
+    } else if(callback){
+		setTimeout(callback, 500);
+	}
+}
+
+typeMessagesSynchronously = (messageQueue) => {
+	let resultFunction;
+	if(messageQueue.length > 0){
+		resultFunction = getTypeWriterFunction(messageQueue, 0);
+	} else{
+		resultFunction = () => {};
+	}
+
+	resultFunction();
+}
+
+getTypeWriterFunction = (messageQueue, index) => {
+	const nextIndex = index + 1;
+	const message = messageQueue[index];
+	if(messageQueue[nextIndex]){
+		return () => {typeWriter(message.text, document.getElementById(message.elementId), 30, 0, getTypeWriterFunction(messageQueue, nextIndex))};
+	} else{
+		return () => {typeWriter(message.text, document.getElementById(message.elementId), 30, 0, null)};
+	}
+}
+
+typeWelcomeMessage = () => {
+	const messageQueue = [
+		{text: 'Hey there!', elementId: 'message-greeting'},
+		{text: 'My name is David. My name is David. My name is David. My name is David. My name is David. My name is David. My name is David. My name is David. My name is David. ', elementId: 'message-body'},
+		{text: 'Sincerely,', elementId: 'message-salutation-line-one'},
+		{text: 'Me', elementId: 'message-salutation-line-two'},
+	]
+
+	typeMessagesSynchronously(messageQueue)
+}
